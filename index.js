8834cad596732d0275888026fe52acb0173f7733
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-03-15 02:52:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`Halo sudah code fix!`);
-  bot.user.setActivity('https://discord.gg/DPQgeFT', { type: 'Playing' });
+  bot.change_presence(game=discord.Game(name="https://discord.gg/DPQgeFT", type=0))
 });
 
 bot.on("message", async message => {
