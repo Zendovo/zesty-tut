0588609077e073ee83113b326a14b5077aec50db
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-08 13:56:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-  setTimeout(() => {
-  bot.user.setActivity("Digunakan server...", {type: "STREAMING"})
   console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("Digunakan server...", {type: "STREAMING"})
   setTimeout(() => {
     bot.user.setActivity(`prefix - | ${bot.guilds.array().length} server`, {type: "WATCHING"});
   }, 20000)
