71eaa9a368e15528675bfebe896465c55d868562
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-09 01:57:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setPresence({ game: { name: 'with depression', type: "streaming", url: "https://www.twitch.tv/"}});
+  bot.user.setPresence({ game: { name: 'Digunakan server', type: "streaming", url: "https://www.twitch.tv/"}});
   bot.user.setStatus('available')
 });
 
