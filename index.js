77211b0418e9a2c548ca9206b14d009e982ce028
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-08 14:06:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,45 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    if(!bUser) return message.channel.send("Can't find user!");
+
+    let bReason = args.join(" ").slice(22);
+
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let banEmbed = new Discord.RichEmbed()
+
+    .setDescription("~Ban~")
+
+    .setColor("#bc0000")
+
+    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+
+    .addField("Banned In", message.channel)
+
+    .addField("Time", message.createdAt)
+
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+
+    incidentchannel.send(banEmbed);
+
+    return;
+
+  }
     
   }
 });
