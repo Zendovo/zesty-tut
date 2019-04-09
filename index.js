a75885942c0022157d07bcd498723ccc6003a371
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-10 06:18:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  user.setActivity("QQ UPDATE NEW!", {type: "<WATCHING, PLAYING, STREAMING, LISTENING>"});
+  <Client>.user.setActivity("QQ UPDATE NEW!", {type: "<WATCHING, PLAYING, STREAMING, LISTENING>"});
   bot.user.setStatus('available')
 });
 
